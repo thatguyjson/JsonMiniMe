d0f9500ab4c1b35f585b2192e03b8424cdb7f956
Json <jasonarodriguez16@gmail.com>
2025-03-08 19:35:10 -0800

    Update grim.py

--- cogs/grim.py
+++ cogs/grim.py
@@ -1,1 +1,13 @@
+'''
+Grim commands cog. This is gonna be used so Grim 
+can maybe make a command or two at some point.
+'''
+class GrimCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
+
+
+
+def setup(bot):
+    bot.add_cog(GrimCog(bot))
