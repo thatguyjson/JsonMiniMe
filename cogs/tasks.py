191c33816d8e72becb094f9874babae4c90d488f
Json <jasonarodriguez16@gmail.com>
2025-03-08 20:41:03 -0800

    created tasks.py for task.loops

---
+++ cogs/tasks.py
@@ -0,0 +1,36 @@
+'''
+HERE ARE ALL IMPORTS NEEDED FOR COGS
+'''
+import nextcord
+import mysql.connector
+import asyncio
+import time
+import random
+import requests
+import json
+import re
+import pytz
+import os
+from nextcord.ext import commands, tasks
+from nextcord.ui import View, Select
+from datetime import datetime, timedelta, timezone
+
+"""
+Constants takes variables from constants.py in Pebblehost and uses that.
+"""
+from constants import *
+from constVariables import *
+from bot import db, cursor
+
+'''
+Tasks cog
+'''
+class TasksCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+
+def setup(bot):
+    bot.add_cog(TasksCog(bot))
