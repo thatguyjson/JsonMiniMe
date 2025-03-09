76ab47f5c200e378d5e0dc0fe03878b05dac96bf
Json <jasonarodriguez16@gmail.com>
2025-03-08 20:43:12 -0800

    added general fun command ?praise

--- cogs/general.py
+++ cogs/general.py
@@ -29,8 +29,24 @@ class GeneralCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-
+    @commands.command()
+    async def praise(self, ctx, member: nextcord.Member = None):
+        if member is None:
+          await ctx.send("Please ping the user you would like to praise!")
+        else:
+          message_index = random.randint(1, 9)
+          messages = {
+                  1: f"{member.mention}, you look so good right now.",
+                  2: f"{member.mention}, I love you.",
+                  3: f"{member.mention}, you deserve a gold star.",
+                  4: f"{member.mention}, youre quite the gem, a lovely one at that.",
+                  5: f"{member.mention}, you always brighten my day.",
+                  6: f"{member.mention}, good job kitten.",
+                  7: f"womp womp {member.mention}, no praise for you :(",
+                  8: f"{member.mention}, you can be my skibidi rizzler.",
+                  9: f"{member.mention}, I would choose you in a garden of roses.",
+            }
+          await ctx.send(messages[message_index])
 
 def setup(bot):
     bot.add_cog(GeneralCog(bot))
