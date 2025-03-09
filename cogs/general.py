65e04b57cdd80a2a380307f76db70114ab3d0435
Json <jasonarodriguez16@gmail.com>
2025-03-08 19:59:37 -0800

    added imports to general.py

--- cogs/general.py
+++ cogs/general.py
@@ -1,4 +1,27 @@
 '''
+HERE ARE ALL IMPORTS NEEDED FOR COGS
+'''
+import nextcord
+import mysql.connector
+import asyncio
+import time
+import random
+import requests
+import json
+import re
+import pytz
+import os
+from nextcord.ext import commands, tasks
+from nextcord.ui import View, Select
+from datetime import datetime, timedelta, timezone
+
+"""
+Constants takes variables from constants.py in Pebblehost and uses that.
+"""
+from constants import *
+from constVariables import *
+
+'''
 General commands cog
 '''
 class GeneralCog(commands.Cog):
