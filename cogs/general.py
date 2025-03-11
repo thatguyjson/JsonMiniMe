7d59e60aff1ab61259637b1a6d38904d919cd46a
Json <jasonarodriguez16@gmail.com>
2025-03-11 15:21:46 -0700

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -11,7 +11,7 @@ import json
 import re
 import pytz
 import os
-from nextcord.ext import commands, tasks, slash_commands
+from nextcord.ext import commands, tasks
 from nextcord.ui import View, Select
 from datetime import datetime, timedelta, timezone
 
@@ -48,7 +48,7 @@ class GeneralCog(commands.Cog):
             }
           await ctx.send(messages[message_index])
 
-    @commands.slash_command(name="ping", description="Ping the bot")
+    @commands.slash_command(guild_ids=[GUILD_ID])
     async def ping(self, interaction: nextcord.Interaction):
         await interaction.response.send_message("Shut the fuck up")
 
