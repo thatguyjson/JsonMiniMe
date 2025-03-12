3bc38e02869679ee2a5cdd9576424d3a8661da5f
Json <jasonarodriguez16@gmail.com>
2025-03-11 22:07:22 -0700

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -32,7 +32,7 @@ async def is_owner(ctx):
 async def is_drip(ctx):
     return ctx.author.id == 639904427624628224
 
-def check(msg):
+def check(ctx, msg):
     return msg.author == ctx.author and msg.channel == ctx.channel
 '''
 Admin commands cog
